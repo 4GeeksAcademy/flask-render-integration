7844d275cb62d1d4b1df724b3bcbb4675377e816
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2023-08-10 09:40:03 +0000

    Added model and template

--- src/app.py
+++ src/app.py
@@ -1,6 +1,29 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from pickle import load
 app = Flask(__name__)
+model = load(open("/workspaces/flask-render-integration/models/decision_tree_classifier_default_42.sav","rb"))
+class_dict = {
+    "0": "Iris setosa",
+    "1": "Iris versicolor",
+    "2": "Iris virginica"
+}
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    prediction = None
+    if request.method == "POST":
+        
+        # Obtain values from form
+        val1 = float(request.form['val1'])
+        val2 = float(request.form['val2'])
+        val3 = float(request.form['val3'])
+        val4 = float(request.form['val4'])
+        
+        data = [[val1, val2, val3, val4]]
+        prediction = str(model.predict(data)[0])
+        print(prediction)
+        pred_class = class_dict[prediction]
+    else:
+        pred_class = None
+    
+    return render_template("index.html", prediction = pred_class)

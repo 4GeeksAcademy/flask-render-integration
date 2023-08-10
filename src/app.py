7aa2ea8849065cdadb6dab7b5f651fa72f417c39
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2023-08-10 15:42:05 +0000

    Styling review of code

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template
 from pickle import load
+
 app = Flask(__name__)
-model = load(open("../models/decision_tree_classifier_default_42.sav","rb"))
+model = load(open("../models/decision_tree_classifier_default_42.sav", "rb"))
 class_dict = {
     "0": "Iris setosa",
     "1": "Iris versicolor",
@@ -13,10 +14,10 @@ def index():
     if request.method == "POST":
         
         # Obtain values from form
-        val1 = float(request.form['val1'])
-        val2 = float(request.form['val2'])
-        val3 = float(request.form['val3'])
-        val4 = float(request.form['val4'])
+        val1 = float(request.form["val1"])
+        val2 = float(request.form["val2"])
+        val3 = float(request.form["val3"])
+        val4 = float(request.form["val4"])
         
         data = [[val1, val2, val3, val4]]
         prediction = str(model.predict(data)[0])
